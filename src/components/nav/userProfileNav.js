fc4c7a95960061308e748b5fa82c9e1803dd8961
Matthew Lockdall <mlockdall26@gmail.com>
2020-03-22 13:57:53 -0500

    redirect added in navs

--- src/components/nav/userProfileNav.js
+++ src/components/nav/userProfileNav.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Redirect } from '@reach/router';
 import NavLink from './NavLink';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faNotesMedical, faUserAstronaut, faChalkboardTeacher, faPenAlt} from '@fortawesome/free-solid-svg-icons';
+import Store from './../../contexts/Store';
+
 const UserProfileNav = () => {
+    const { user } = useContext(Store);
     return (
+        user.isAuthenticated ?
         <nav>
             <NavLink to="/"><FontAwesomeIcon icon={faChalkboardTeacher} size="lg"/></NavLink>
             <NavLink to=""><FontAwesomeIcon icon={faUserAstronaut} size="lg"/></NavLink>
             <NavLink to="editUser"><FontAwesomeIcon icon={faPenAlt}/> <FontAwesomeIcon icon={faUserAstronaut} size="lg"/></NavLink>
             <NavLink to="userNote"><FontAwesomeIcon icon={faNotesMedical} size="lg"/></NavLink>
         </nav>
+        :
+        <Redirect to="/" />
     )
 }
 export default UserProfileNav;

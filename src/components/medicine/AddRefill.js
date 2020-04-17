03cbdbb15cf7dba883c9a7cb466d7796eb26b177
Matthew Lockdall <mlockdall26@gmail.com>
2020-04-17 16:20:31 -0500

    url match

--- src/components/medicine/AddRefill.js
+++ src/components/medicine/AddRefill.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import RefillForm from './../form/RefillForm';
-const AddRefill = ({med}) => {
-    console.log(med);
-    
+const AddRefill = ({ med }) => {
+    console.log('med', med);
+
     return (
         <main>
-            <RefillForm
-                med={med} />
+            <RefillForm med={med} />
         </main>
-    )
-}
+    );
+};
 export default AddRefill;
--- src/components/medicine/MedicineRefill.js
+++ src/components/medicine/MedicineRefill.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const MedicineRefill = ({item}) => {
+const MedicineRefill = ({ item }) => {
     return (
         <div key={item.id}>
             <header>{item.date}</header>
-                <p>{item.details}</p>
+            <p>{item.details}</p>
         </div>
-    )
-}
+    );
+};
 export default MedicineRefill;

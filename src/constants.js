0f28befd5c7c0c453c52b89359234e31199cceab
Matthew Lockdall <mlockdall26@gmail.com>
2020-04-17 15:39:51 -0500

    updated baseUrl handling

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,11 @@
-// const BASE_URL= `http://localhost:3000`;
-
-const BASE_URL = `https://diabetes-supplies-inventory.herokuapp.com`;
+const baseUrl = () => {
+    if (window.location.href.indexOf('localhost') > -1) {
+        return `http://localhost:3000`;
+    } else {
+        return `https://diabetes-supplies-inventory.herokuapp.com`;
+    }
+};
+// console.log('bob', baseUrl());
+const BASE_URL = baseUrl();
 
 export default BASE_URL;
